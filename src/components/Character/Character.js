324e02103dba36f0f69fcd62956d4a7611f66561
debosch <vlad.shiltsin@gmail.com>
2021-10-11 16:03:15 +0600

    added alt to img

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -4,12 +4,12 @@ import './character.scss'
 
 const Character = (props) => {
     return (
-        <div className="character-card">
+        <a className="character-card">
             <div className="character-card__image-wrapper">
-                <img className="character-card__image" width="300" height="400" src="https://via.placeholder.com/300x400"></img>
+                <img alt={ "Image of " + props.name } className="character-card__image" width="300" height="400" src="https://via.placeholder.com/300x400"></img>
             </div>
             <p className="character-card__character-name">{ props.name }</p>
-        </div>
+        </a>
     );
 };
 
