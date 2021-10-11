5d1fe9b24dc0b7e85fc0db411e49facfdc53c607
debosch <vlad.shiltsin@gmail.com>
2021-10-11 18:06:56 +0600

    reduntant imports are deleted

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import './character.scss'
 
 const Character = (props) => {
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 import './searchbar.scss';
 
 const SearchBar = () => {
